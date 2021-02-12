2f14b251fd1c8aeeeaa987ca408b0a965d1be05b
Ben Kaminski <ben@benkaminski.com>
2021-02-12 16:09:29 -0500

    security fixes

--- front-page.php
+++ front-page.php
@@ -1,6 +1,4 @@
 <?php get_header(); ?>
-<header>
-	<?php get_template_part('parts/home', 'hero'); ?>
-</header>
+
 
 <?php get_footer(); ?>
--- header.php
+++ header.php
@@ -1,10 +1,13 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
+
 <head>
 <meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<?php get_template_part( 'parts/page', 'icons' ); ?>
-<?php wp_head(); ?>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+	<?php get_template_part( 'parts/page', 'icons' ); ?>
+
+	<?php wp_head(); ?>
+
 </head>
 <body <?php body_class(); ?>>
 <?php get_template_part( 'parts/page', 'navbar' ); ?>
